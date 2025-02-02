d49329cd76ceef5ec6ec15c7036a4227b85a4aba
ahsanul0185 <ahsanul0185@gmail.com>
2025-02-03 03:12:50 +0600

    added link to the buttons

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -9,12 +9,16 @@ function Main() {
       <Kauppatiede />
 
       <div className="flex flex-col sm:flex-row items-center justify-center mx-auto gap-3 sm:gap-5 max-w-xl w-full">
-        <button className="text-sm lg:text-base px-2 bg-black w-full sm:flex-1 h-16 sm:h-20 md:h-36 lg:h-40 rounded-xl grid place-items-center">
+
+        <button onClick={() => window.location.href = "https://valmennus-kurssi-openai.netlify.app/"} className="text-sm lg:text-base px-2 bg-black w-full sm:flex-1 h-16 sm:h-20 md:h-36 lg:h-40 rounded-xl grid place-items-center">
           Taloustieto
         </button>
-        <button className="text-sm lg:text-base px-2 bg-black w-full sm:flex-1 h-16 sm:h-20 md:h-36 lg:h-40 rounded-xl grid place-items-center">
+
+        
+        <button onClick={() => window.location.href = "https://valmennus-kurssi-openai.netlify.app/"} className="text-sm lg:text-base px-2 bg-black w-full sm:flex-1 h-16 sm:h-20 md:h-36 lg:h-40 rounded-xl grid place-items-center">
           Tilasto ja todennäköisyys
         </button>
+ 
       </div>
     </div>
   );
